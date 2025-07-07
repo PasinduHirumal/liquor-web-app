30bc8c82d911a5ed19269a38d53950a607d01a6d
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-07-07 09:34:18 +0530

    change frontend port

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ dotenv.config();
 
 
 const app = express();
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
 
