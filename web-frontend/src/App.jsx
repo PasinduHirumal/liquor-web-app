47f90157df624637d5b5116957d057525a04229c
Pasindu Hirumal <pasinduhirumal7@gmail.com>
2025-07-09 13:58:41 +0530

    register done

--- web-frontend/src/App.jsx
+++ web-frontend/src/App.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import LoginForm from "./Login";
+import RegisterForm from "./Register";
+
+const Home = () => {
+  return (
+    <div className="container mt-4">
+      <h1>Welcome Home!</h1>
+      <p>You have successfully registered and logged in.</p>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div>
-      <h1>ghi</h1>
-    </div>
+    <Router>
+      <Routes>
+        {/* Home page */}
+        <Route path="/" element={<Home />} />
+
+        {/* Default redirect to login */}
+        <Route path="/login" element={<LoginForm />} />
+
+        {/* Register page */}
+        <Route path="/register" element={<RegisterForm />} />
+
+        {/* Redirect unknown routes to login */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
