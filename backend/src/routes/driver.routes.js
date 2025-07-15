021e4fdf0a8c58301a762ce5ede17e5020685bb7
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-07-15 14:34:22 +0530

    delete driver account

--- backend/src/routes/driver.routes.js
+++ backend/src/routes/driver.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { authenticateUser, authorizeRoles } from '../middleware/authMiddleware.js';
 import { validateDriver } from '../validations/DriverValidator.js';
-import { createDriver } from '../controller/driver.controller.js';
+import { createDriver, deleteDriver } from '../controller/driver.controller.js';
 
 
 const router = express.Router();
@@ -9,5 +9,6 @@ const router = express.Router();
 // http://localhost:5000/api/drivers
 
 router.post('/createDriver', authenticateUser, authorizeRoles("super_admin", "admin"), validateDriver, createDriver);
+router.delete('/delete/:id', authenticateUser, authorizeRoles("super_admin"), deleteDriver);
 
 export default router;
