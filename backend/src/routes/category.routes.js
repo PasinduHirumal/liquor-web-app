c5248c50bc63a95a2b85d95bbd45d2fbe12ecbd3
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-07-17 11:31:12 +0530

    delete category

--- backend/src/routes/category.routes.js
+++ backend/src/routes/category.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { authenticateUser, authorizeRoles } from '../middleware/authMiddleware.js';
-import { createCategory, getAllCategories, updateCategory } from '../controller/category.controller.js';
+import { createCategory, deleteCategory, getAllCategories, updateCategory } from '../controller/category.controller.js';
 import { validateCategory, validateCategoryUpdate } from '../validations/CategoryValidator.js';
 
 const router = express.Router();
@@ -10,5 +10,6 @@ const router = express.Router();
 router.get('/getAll', getAllCategories);
 router.post('/create', authenticateUser, authorizeRoles("admin", "super_admin"), validateCategory, createCategory);
 router.patch('/update/:id', authenticateUser, authorizeRoles("admin", "super_admin"), validateCategoryUpdate, updateCategory);
+router.delete('/delete/:id', authenticateUser, authorizeRoles("super_admin"), deleteCategory);
 
 export default router;
