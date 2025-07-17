68cfc59d1e2483a40ef0b2d676350e19ca31a364
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-07-17 11:23:06 +0530

    update category

--- backend/src/controller/category.controller.js
+++ backend/src/controller/category.controller.js
@@ -57,4 +57,24 @@ const getAllCategories = async (req, res) => {
     }
 };
 
-export { createCategory, getAllCategories, };
+const updateCategory = async (req, res) => {
+	try {
+        const categoryId = req.params.id;
+
+        const category = await categoryService.findById(categoryId);
+        if (!category) {
+            return res.status(404).json({ success: false, message: "Category not found"});
+        }
+        
+        const categoryData = { ...req.body };
+
+        const updatedCategory = await categoryService.updateById(categoryId, categoryData);
+
+        return res.status(200).json({ success: true, message: "Category updated successfully", data: updatedCategory });
+    } catch (error) {
+        console.error("Method_name error:", error.message);
+        return res.status(500).json({ success: false, message: "Server Error" });
+    }
+};
+
+export { createCategory, getAllCategories, updateCategory, };
--- backend/src/routes/category.routes.js
+++ backend/src/routes/category.routes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { authenticateUser, authorizeRoles } from '../middleware/authMiddleware.js';
-import { createCategory, getAllCategories } from '../controller/category.controller.js';
-import { validateCategory } from '../validations/CategoryValidator.js';
+import { createCategory, getAllCategories, updateCategory } from '../controller/category.controller.js';
+import { validateCategory, validateCategoryUpdate } from '../validations/CategoryValidator.js';
 
 const router = express.Router();
 
 // http://localhost:5000/api/categories
 
-router.post('/create', authenticateUser, authorizeRoles("admin", "super_admin"), validateCategory, createCategory);
 router.get('/getAll', getAllCategories);
+router.post('/create', authenticateUser, authorizeRoles("admin", "super_admin"), validateCategory, createCategory);
+router.patch('/update/:id', authenticateUser, authorizeRoles("admin", "super_admin"), validateCategoryUpdate, updateCategory);
 
 export default router;
