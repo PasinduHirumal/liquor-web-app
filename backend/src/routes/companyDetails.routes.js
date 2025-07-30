5deaf9fe7d18bbd5bbcc6bc4edf4ab9d396512e1
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-07-30 14:07:30 +0530

    update system detail

--- backend/src/routes/companyDetails.routes.js
+++ backend/src/routes/companyDetails.routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { authenticateUser, authorizeRoles } from '../middleware/authMiddleware.js';
-import { getCompanyDetails } from '../controller/companyDetails.controller.js';
+import { getCompanyDetails, updateCompanyDetailById } from '../controller/companyDetails.controller.js';
+import { validateCompanyDetailsUpdate } from '../validations/CompanyDetailsValidator.js';
 
 const router = express.Router();
 
 // http://localhost:5000/api/system
 
 router.get('/details', authenticateUser, authorizeRoles("admin", "super_admin"), getCompanyDetails);
+router.patch('/update/:id', authenticateUser, authorizeRoles("super_admin"), validateCompanyDetailsUpdate, updateCompanyDetailById);
 
 export default router;
