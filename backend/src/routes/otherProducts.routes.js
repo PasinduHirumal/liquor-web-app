8494eaf23bd14e6dcf9f351a53ae8c931d2d26c2
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2025-09-02 12:28:57 +0530

    updated

--- backend/src/routes/otherProducts.routes.js
+++ backend/src/routes/otherProducts.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import ADMIN_ROLES from '../enums/adminRoles.js';
 import { authenticateUser, authorizeRoles } from '../middleware/authMiddleware.js';
-import { createProduct, deleteProduct, getAllProducts, getProductById, migrateSearchTokens, updateProduct } from '../controller/otherProducts.controller.js';
+import { createProduct, deleteProductById, getAllProducts, getProductById, migrateSearchTokens, updatePriceById, updateProductById, updateStockById } from '../controller/otherProducts.controller.js';
 import { validateOtherProduct, validateOtherProductUpdate, validatePriceOperation, validateQuantityOperation } from '../validations/OtherProductValidator.js';
 import { searchProductAdvanced } from '../controller/search.controller.js';
 
@@ -16,10 +16,10 @@ router.get('/getAll', getAllProducts);
 router.get('/getAll/dashboard', authenticateUser, authorizeRoles(admin, super_admin), getAllProducts);
 router.get('/getOtherProductById/:id', authenticateUser, authorizeRoles(admin, super_admin), getProductById);
 router.post('/create', authenticateUser, authorizeRoles(admin, super_admin), validateOtherProduct, createProduct);
-router.patch('/update/:id', authenticateUser, authorizeRoles(admin, super_admin), validateOtherProductUpdate, updateProduct);
-router.patch('/update-price/:id', authenticateUser, authorizeRoles(admin, super_admin), validatePriceOperation, updateProduct);
-router.patch('/update-quantity/:id', authenticateUser, authorizeRoles(admin, super_admin), validateQuantityOperation, updateProduct);
-router.delete('/delete/:id', authenticateUser, authorizeRoles(super_admin), deleteProduct);
+router.patch('/update/:id', authenticateUser, authorizeRoles(admin, super_admin), validateOtherProductUpdate, updateProductById);
+router.patch('/update-price/:id', authenticateUser, authorizeRoles(admin, super_admin), validatePriceOperation, updatePriceById);
+router.patch('/update-quantity/:id', authenticateUser, authorizeRoles(admin, super_admin), validateQuantityOperation, updateStockById);
+router.delete('/delete/:id', authenticateUser, authorizeRoles(super_admin), deleteProductById);
 
 //  Run this end-point once to add search tokens to existing documents after create & update
 router.post('/migrate', authenticateUser, authorizeRoles(admin, super_admin), migrateSearchTokens);
