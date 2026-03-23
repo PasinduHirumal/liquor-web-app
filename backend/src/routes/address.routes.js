47eaafec42522bc6369b0330f3d5fff429a349eb
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2026-03-23 17:08:17 +0530

    up

--- backend/src/routes/address.routes.js
+++ backend/src/routes/address.routes.js
@@ -2,13 +2,16 @@ import express from 'express';
 import { authenticateUser, authorizeRoles } from '../middleware/authMiddleware.js';
 import { validateAddress, validateAddressUpdate } from '../validations/AddressValidator.js';
 import { createAddress, getAllAddressesForUser, updateAddress } from '../controller/address.controller.js';
+import USER_ROLES from '../enums/userRoles.js';
 
 const router = express.Router();
 
+const user = USER_ROLES.USER;
+
 // http://localhost:5000/api/addresses
 
-router.post('/createAddress', authenticateUser, validateAddress, createAddress);
+router.post('/createAddress', authenticateUser, authorizeRoles(user), validateAddress, createAddress);
 router.get('/getAddressesByUserId/:id', authenticateUser, getAllAddressesForUser);
-router.patch('/update/:id', authenticateUser, validateAddressUpdate, updateAddress);
+router.patch('/update/:id', authenticateUser, authorizeRoles(user), validateAddressUpdate, updateAddress);
 
 export default router;
