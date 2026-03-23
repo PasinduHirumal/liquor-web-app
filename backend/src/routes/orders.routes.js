90227f82858032ea0dbbd354a5ce3462549f7b3c
Imesh Chandula <117582263+ImeshChandula@users.noreply.github.com>
2026-03-23 12:12:10 +0530

    up

--- backend/src/routes/orders.routes.js
+++ backend/src/routes/orders.routes.js
@@ -3,9 +3,11 @@ import ADMIN_ROLES from '../enums/adminRoles.js';
 import { authenticateUser, authorizeRoles } from '../middleware/authMiddleware.js';
 import { validateOrderAssignedDriverUpdate, validateOrderStatusUpdate } from '../validations/OrdersValidator.js';
 import { assignDriverForOrderById, getAllOrders, getOrderById, updateOrderStatusById } from '../controller/orders.controller.js';
+import USER_ROLES from '../enums/userRoles.js';
 
 const router = express.Router();
 
+const user = USER_ROLES.USER;
 const admin = ADMIN_ROLES.ADMIN;
 const super_admin = ADMIN_ROLES.SUPER_ADMIN;
 
@@ -16,4 +18,6 @@ router.get('/getOrderById/:id', authenticateUser, authorizeRoles(admin, super_ad
 router.patch('/update-status/:id', authenticateUser, authorizeRoles(admin, super_admin), validateOrderStatusUpdate, updateOrderStatusById);
 router.patch('/update-assigned_driver/:id', authenticateUser, authorizeRoles(admin, super_admin), validateOrderAssignedDriverUpdate, assignDriverForOrderById);
 
+// user part
+//router.post('/create', authenticateUser, authorizeRoles(user), )
 export default router;
